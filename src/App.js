77fb69134d12d105f139fc05bda4d20607a1035a
Christoffer Hennie <christoffer.hennie@outlook.com>
2020-04-04 14:21:08 +0200

    Stil working on score system
    The score board is implementet but nor updating

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from "react";
 import Choices from "./components/Choices";
 import DisplayImages from "./components/DisplayImages";
 import Result from "./components/Result";
+import Score from "./components/Score";
 
 export default class App extends Component {
   state = {
     userChoice: "paper",
     compChoice: "paper",
     result: "",
+    playerScore: 0,
+    compScore: 0,
     options: [
       {
         name: "rock",
@@ -25,22 +28,56 @@ export default class App extends Component {
   };
 
   updateUserChoice = (e) => {
-    this.setState({userChoice: e, result: ""}, () => {
-      console.log(this.state.userChoice);
-    });
+    this.setState({userChoice: e, result: ""}, () =>
+      console.log(this.state.userChoice)
+    );
   };
 
   result = (compChoice, result) => {
-    this.setState(compChoice, result, () =>
-      console.log(this.state.result)
-    );
+    this.updateScore();
+    this.setState(compChoice, result, () => {
+      console.log(this.state.result);
+    });
+  };
+
+  updateScore = (result = this.state.result) => {
+    switch (result) {
+      case "win":
+        return this.setState(
+          (prevPlayerScore = this.state.playerScore) => {
+            return (
+              {
+                playerScore: prevPlayerScore++,
+              },
+              () => console.log(this.state.compScore)
+            );
+          }
+        );
+      case "lose":
+        return this.setState(
+          (prevCompScore = this.state.compScore) => {
+            return (
+              {
+                compScore: prevCompScore++,
+              },
+              () => console.log(this.state.compScore)
+            );
+          }
+        );
+      default:
+        return console.log("tie");
+    }
   };
 
   render() {
     return (
       <div className='App'>
         <h1>Rock Paper Scissors</h1>
-        <div className="options">
+        <Score
+          playerScore={this.state.playerScore}
+          compScore={this.state.compScore}
+        />
+        <div className='options'>
           <Choices
             options={this.state.options}
             choice={this.updateUserChoice}
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
 
 export default class Score extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='parent-score-board'>
+        <div className='score-board'>
+          <p className='opponents'>Player</p>
+          <p className='score'>
+            {this.props.playerScore}:{this.props.compScore}
+          </p>
+          <p className='opponents'>Comp</p>
+        </div>
+      </div>
+    );
+  }
 }
