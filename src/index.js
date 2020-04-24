7fab7890846c0d5ed8d0b7c88348a9d47f6374db
Christoffer Hennie <44028914+ChristofferHennie@users.noreply.github.com>
2020-04-24 13:55:32 +0200

    Added serviceworker to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+imprort * as serviceWorker from './serviceworker';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
